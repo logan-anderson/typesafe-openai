ebdb251d14d633e35034057dddbfe4c4a90a36a1
Logan Anderson <landerson2@upei.ca>
2023-07-13 18:39:20 -0400

    add working example and functionForce

---
+++ src/example.ts
@@ -0,0 +1,70 @@
+import { Configuration, OpenAIApi } from "openai";
+import z from "zod";
+import dotenv from "dotenv";
+dotenv.config();
+import { TypeSafeOpenAIApi } from "./typesafeOpenAi";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new TypeSafeOpenAIApi(configuration);
+
+const run = async () => {
+  // New Way
+  const res = await openai.createChatCompletionTypeSafe({
+    messages: [
+      {
+        role: "user",
+        content: "What is the weather like today in Washington DC?",
+      },
+    ],
+    model: "gpt-3.5-turbo-0613",
+    function_call: { name: "getWeather" },
+    functionForce: {
+      name: "getWeather",
+      description: "Get the current weather",
+      parameters: z.object({
+        location: z.string().describe("The location to get the weather for"),
+        format: z
+          .enum(["F", "C"])
+          .describe("The format to return the weather in."),
+      }),
+    },
+    // functions: [
+    //   {
+    //     name: "getWeather",
+    //     description: "Get the current weather",
+    //     parameters: z.object({
+    //       location: z.string().describe("The location to get the weather for"),
+    //       format: z
+    //         .enum(["F", "C"])
+    //         .describe("The format to return the weather in."),
+    //     }),
+    //   },
+    // ],
+  });
+  console.dir({ res }, { depth: null });
+};
+
+run();
+// // Old way
+// openai.createChatCompletion({
+//   messages: [{ role: "user", content: "What is the weather like today?" }],
+//   model: "gpt-3.5-turbo-0613",
+//   functions: [
+//     {
+//       name: "weather",
+//       description: "Get the current weather",
+//       parameters: {
+//         type: "object",
+//         properties: {
+//           location: {
+//             type: "string",
+//             description: "The location to get the weather for",
+//           },
+//         },
+//       },
+//     },
+//   ],
+// });
---
+++ src/typesafeOpenAi.ts
@@ -0,0 +1,76 @@
+import { OpenAIApi } from "openai";
+import type { ZodSchema, TypeOf } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
+import type { ChatCompletionFunctions } from "openai";
+
+// This way of openAI updates the types ours will automatically update and we will get type errors if we do something wrong
+type CreateChatCompletionTypeSafeFunctionArgs = Parameters<
+  OpenAIApi["createChatCompletion"]
+>;
+
+interface OpenAiFunctionWithParameters<Z extends ZodSchema = ZodSchema> {
+  name: string;
+  description?: string;
+  parameters: Z;
+}
+type FunctionArray = OpenAiFunctionWithParameters[];
+interface FunctionsArg {
+  functions: FunctionArray;
+}
+interface FunctionForceArg<Z extends ZodSchema> {
+  functionForce: OpenAiFunctionWithParameters<Z>;
+}
+
+export class TypeSafeOpenAIApi extends OpenAIApi {
+  constructor(configuration) {
+    super(configuration);
+  }
+  async createChatCompletionTypeSafe<Z extends ZodSchema>(
+    args: Omit<CreateChatCompletionTypeSafeFunctionArgs[0], "functions"> &
+      (FunctionsArg | FunctionForceArg<Z> | null),
+    options?: CreateChatCompletionTypeSafeFunctionArgs[1]
+  ): Promise<TypeOf<Z>> {
+    if ("functionForce" in args) {
+      const { functionForce, ...rest } = args;
+      const functions = [
+        {
+          name: args.functionForce.name,
+          description: args.functionForce.description,
+          parameters: zodToJsonSchema(args.functionForce.parameters),
+        },
+      ];
+
+      const res = await this.createChatCompletion(
+        { ...rest, functions },
+        options
+      );
+      const arr = res?.data?.choices || [];
+      const functionArgs = JSON.parse(
+        arr[0]?.message?.function_call?.arguments || "{}"
+      );
+
+      return functionArgs as TypeOf<Z>;
+    } else if ("functions" in args) {
+      const functions: ChatCompletionFunctions[] =
+        args?.functions?.map((fn) => {
+          return {
+            name: fn.name,
+            description: fn.description,
+            parameters: zodToJsonSchema(fn.parameters),
+          };
+        }) || [];
+      console.dir(functions, { depth: null });
+      const res = await this.createChatCompletion(
+        { ...args, functions },
+        options
+      );
+      return res;
+      // const arr = res?.data?.choices || [];
+      // const foo = JSON.parse(arr[0]?.message?.function_call?.arguments || "{}");
+
+      // return foo as TypeOf<Z>;
+    } else {
+      return this.createChatCompletion(args, options);
+    }
+  }
+}
